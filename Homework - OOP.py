cb0c2005e0245844611b312bda6ba6b5d333765e
Mahmoud Mahmoud <1906425@brunel.ac.uk>
2021-02-02 21:22:12 +0000

    You can now print out the enemy's attributes as well as damage output from their assigned weapons. I have also started the challlenge exercise

--- Homework - OOP.py
+++ Homework - OOP.py
@@ -1,3 +1,7 @@
+import random
+
+enemy_count = 0
+
 class Enemies:
 
     weapons = {"Gun": 20, "Knife":15, "Poison":10}
@@ -10,23 +14,39 @@ class Enemies:
 
     def set_description(self, description):
         self.description = description
-        pass
 
     def get_description(self):
-        return self.description
+        return f""" This enemy's name is {self.name} they are from the {self.clan} clan,
+and their ability is: {self.ability} {self.description}"""
 
 
     def set_weapon(self, weapon):
         self.weapon = weapon
+        self.damage = Enemies.weapons[self.weapon]
+
+    def get_weapon(self):
+        return f" Weapon:{self.weapon}, Damage: {self.damage} "
 
 
 
+# enemy1 = Enemies("Grunt", "A", "Super strength")
+# enemy1.set_description("This enemy has four arms, it looks like it can multi task")
+# enemy1.set_weapon("Knife")
+# print(enemy1.get_description())
+# print(enemy1.get_weapon())
+#
+#
+# enemy2 = Enemies("Bodyguard", "B", "Regeneration")
+# enemy2.set_description("This enemy looks pretty average.")
+# enemy2.set_weapon("Gun")
+# print(enemy2.get_description())
+# print(enemy2.get_weapon())
+#
 
-enemy1 = Enemies("Grunt", "A", "Super strength")
-enemy1.set_description("This enemy has four arms, it looks like it can multi task")
-enemy1.set_weapon("Gun")
 
+list_of_enemies =[]
 
-enemy2 = Enemies("Bodyguard", "B", "Regeneration")
-enemy2.set_description("This enemy looks pretty average.")
-enemy2.set_weapon("Knife")
+while enemy_count<5:
+    list_of_enemies.append(Enemies.weapons)
+    enemy_count += 1
+    print(list_of_enemies)
